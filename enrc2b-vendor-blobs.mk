53e391ca1aa003ef2200dc4ab2c5b4b886f061d1
Tom <tom@decompile.me.uk>
2013-06-07 13:32:52 +0100

    added missing bluetooth file

--- enrc2b-vendor-blobs.mk
+++ enrc2b-vendor-blobs.mk
@@ -50,6 +50,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc/enrc2b/proprietary/bin/poweron_modem_hboot.sh:system/bin/poweron_modem_hboot.sh \
   vendor/htc/enrc2b/proprietary/bin/rild:system/bin/rild \
   vendor/htc/enrc2b/proprietary/bin/tf_daemon:system/bin/tf_daemon \
+  vendor/htc/enrc2b/proprietary/etc/firmware/BCM4334B0_002.001.013.0767.0777.hcd:system/etc/firmware/BCM4334B0_002.001.013.0767.0777.hcd \
   vendor/htc/enrc2b/proprietary/etc/firmware/fw_bcm4334_apsta.bin:system/etc/firmware/fw_bcm4334_apsta.bin \
   vendor/htc/enrc2b/proprietary/etc/firmware/fw_bcm4334.bin:system/etc/firmware/fw_bcm4334.bin \
   vendor/htc/enrc2b/proprietary/etc/firmware/fw_bcm4334_p2p.bin:system/etc/firmware/fw_bcm4334_p2p.bin \
